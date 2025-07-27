da773447fc414b4fa0dc1798aa150608c15d5c69
sedat23 <sedatcetin23@gmail.com>
2025-07-27 16:55:18 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.getElementById("iletisimForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Mesajınız gönderildi! Teşekkür ederiz.");
+});
+document.getElementById("iletisimForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Teşekkürler! Mesajınız başarıyla gönderildi.");
+});
+
